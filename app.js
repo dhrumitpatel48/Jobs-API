cf533b1c88837bed9e4ffaf93e23176ad9293c5c
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-06-03 22:01:03 +0530

    Completed general setup of project

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+// Connect DB
+const connectDB = require("./db/connect")
+
+// routes
+const authRouter = require("./routes/auth.route");
+const jobsRouter = require("./routes/jobs.route");
+
+// error handler
+const notFound = require("./middleware/not-found.middleware");
+const errorHandler = require("./middleware/error-handler.middleware");
+
+app.use(express.json());
+
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is runnning of port : ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ errors/unauthenticated.js
@@ -0,0 +1,11 @@
+const { StatusCodes } = require("http-require-codes");
+const CustomAPIError = require("./custom-api");
+
+class UnauthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+module.exports = UnauthenticatedError;
