46658a44958ce93b1ce3ac8cb20ea7ff87bf6b3f
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-06-04 12:56:22 +0530

    added auth middleware

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // Connect DB
-const connectDB = require("./db/connect")
+const connectDB = require("./db/connect");
 
 // routes
 const authRouter = require("./routes/auth.route");
@@ -15,11 +15,13 @@ const jobsRouter = require("./routes/jobs.route");
 const notFound = require("./middleware/not-found.middleware");
 const errorHandler = require("./middleware/error-handler.middleware");
 
+const authenticateUser = require("./middleware/authentication.middleware");
+
 app.use(express.json());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -28,7 +30,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is runnning of port : ${port}`));
   } catch (error) {
     console.log(error);
