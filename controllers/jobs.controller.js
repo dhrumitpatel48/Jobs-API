6369a999ae8e95b5dee71c7cbf04d7ee8dfbb8d9
Dhrumit Patel <80308003+dhrumitt@users.noreply.github.com>
2023-06-06 22:42:23 +0530

    Get All Jobs Completed (#10)

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,5 +1,11 @@
+const Job = require("../models/Job.model");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors/bad-request");
+const { NotFoundError } = require("../errors/not-found");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: (await jobs).length });
 };
 
 const getJob = async (req, res) => {
@@ -7,7 +13,9 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.send("createJob");
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
