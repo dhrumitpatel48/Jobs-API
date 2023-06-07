f9392ec846af830216899c305ba3eb84e0696df6
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-06-07 21:54:12 +0530

    Completed update job and delete job

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -28,11 +28,34 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send("updateJob");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or position fields can't be empty");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) throw new NotFoundError(`No Job with jobid: ${jobId}`);
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) throw new NotFoundError(`No Job with jobid: ${jobId}`);
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
