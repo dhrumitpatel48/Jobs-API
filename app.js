71b7e6e460ac0235ba888bf01b3cb1a44d3f69bf
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-06-10 12:38:35 +0530

    Added security precautions

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -17,7 +23,17 @@ const errorHandler = require("./middleware/error-handler.middleware");
 
 const authenticateUser = require("./middleware/authentication.middleware");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
